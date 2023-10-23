27165d05d0c84f196d49fe2a7b3ecf20793c231f
Joel Huckle <joelhuckle2004@gmail.com>
2023-10-23 09:19:25 +0100

    edit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+const shows = ['family guy', 'breaking bad', 'office']
+shows.forEach(n => console.log(n));
 
+const numbers = [1,2,3,4,5,6,7,8,9]
+console.log(numbers.filter(n => n%2 === 0));
+
+let arrayTest =[1,6,5,3]
+
+function sumSecondLowAndHigh(arr){
+    arr = arr.sort()
+    return arr[1] + arr[arr.length - 2];
+}
+
+console.log(sumSecondLowAndHigh(arrayTest));
